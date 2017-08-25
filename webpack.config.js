3414aa913767022ff5d399ce468547f740cdab14
Tim Shelburne <shelburt02@gmail.com>
2017-08-25 16:58:00 -0400

    added frontend client building

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	devtool: `eval-cheap-module-source-map`,
+	entry: `./src/client.js`,
+	output: {
+		path: `${__dirname}/public/js`,
+		filename: `app.js`
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: `babel-loader` }
+		]
+	}
+}
