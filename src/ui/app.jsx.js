72c6fd4a30691fbc8219bb661602889517ce3f24
Tim Shelburne <shelburt02@gmail.com>
2017-08-29 15:18:00 -0400

    moved module 0 code into module 0 UI module

--- src/ui/app.jsx.js
+++ src/ui/app.jsx.js
@@ -1,29 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Module from './modules/module-0.jsx'
 
-const App = ({registration: {status, message}, actions}) =>
-	<div>
-		<h1>Almost there!</h1>
-		<p>
-			Congratulations on making it this far! If you've set everything up properly,
-			clicking the button below should register you with our system as having
-			finished the module 0 exercise.
-		</p>
-		<button disabled={status === `sending` || status === `success`} onClick={actions.register}>Click Me</button>
-
-		{isCompleted(status) && <p className={status}>{message}</p>}
-	</div>
-
-App.propTypes = {
-	registration: PropTypes.shape({
-		status: PropTypes.oneOf([`init`, `sending`, `success`, `failure`]).isRequired,
-		message: PropTypes.string.isRequired,
-	}),
-	actions: PropTypes.object.isRequired,
-}
+const App = props => <Module {...props} />
 
 export default App
-
-function isCompleted(status) {
-	return status === `success` || status === `failure`
-}
