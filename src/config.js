3189759a781bf73a25e26d01f3668720ededa2b5
Tim Shelburne <shelburt02@gmail.com>
2017-08-29 12:54:12 -0400

    fixes for damn knex - ugh

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-const config = {
+module.exports = {
 	env: process.env.ENV || `development`,
 	studentId: process.env.STUDENT_ID,
 	backend: process.env.BACKEND || `localhost:${process.env.PORT}`,
 	port: process.env.PORT || 3000,
 	db: process.env.DATABASE_URL,
 }
-
-export default config
