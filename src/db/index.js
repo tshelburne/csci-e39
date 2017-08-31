d095b01bb4aa5ee213d5b64f719136458334438d
Tim Shelburne <shelburt02@gmail.com>
2017-08-31 07:36:09 -0400

    created upload model

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,17 @@
 import db from './connection'
 
-export const Student = db.Model.extend({tableName: `students`})
+export const Student = db.Model.extend({
+	tableName: `students`,
+
+	uploads() {
+		return this.hasMany(Upload, `creator_id`)
+	},
+})
+
+export const Upload = db.Model.extend({
+	tableName: `uploads`,
+
+	student() {
+		return this.belongsTo(Student, `creator_id`)
+	},
+})
