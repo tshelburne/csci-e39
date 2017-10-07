687c63919fe539de42c60195340a8f8d63055faa
Natalya Shelburne <natalyathree@gmail.com>
2017-10-07 10:26:32 -0400

    initial styles created

--- src/assignments/module-0/index.jsx.js
+++ src/assignments/module-0/index.jsx.js
@@ -2,16 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Module0 = ({registration: {status, message}, actions}) =>
-	<div>
-		<h1>Almost there!</h1>
-		<p>
-			Congratulations on making it this far! If you've set everything up properly,
-			clicking the button below should register you with our system as having
-			finished the module 0 exercise.
-		</p>
-		<button disabled={status === `pending` || status === `success`} onClick={actions.register}>Click Me</button>
+	<div className="module0-container">
+		<header className="header">
+			CSCI-e39
+		</header>
+		<section className="submission-section">
+			<h1 className="main-message">Almost there!</h1>
+			<p>
+				Congratulations on making it this far! If you've followed the directions and set up the project,
+				clicking the button below should register you with our system as having
+				finished the module 0 exercise.
+			</p>
+			<button disabled={status === `pending` || status === `success`}
+				onClick={actions.register}
+				className="call-to-action">
+				Complete Assignment
+			</button>
 
-		{isCompleted(status) && <p className={status}>{message}</p>}
+			{isCompleted(status) && <p className={status}>{message}</p>}
+		</section>
 	</div>
 
 Module0.propTypes = {
