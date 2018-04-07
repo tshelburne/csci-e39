5d61c96636df02c00599331f282eb5eaac3648cf
Curtis Wilcox <github.com@m.cognize.org>
2018-04-07 18:03:16 -0400

    restore key property to list element

--- src/assignments/project-1/components/completedFiles.jsx.js
+++ src/assignments/project-1/components/completedFiles.jsx.js
@@ -5,7 +5,7 @@ const CompletedFiles = ({completedFiles}) => {
 			{completedFiles.map(file => {
 				const {id, name, url, error} = file
 
-				return <li>
+				return <li key={id}>
 					<span id={id}>{name}</span>
 					{!error && <img src={url} style={{maxWidth: `200px`}} aria-labelledby={id}/>}
 					{!!error && <p className="failure">{error}</p>}
