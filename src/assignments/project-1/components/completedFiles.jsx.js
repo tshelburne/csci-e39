9ff262918ea5e6dc0f6906b426163600fc14cc9e
Curtis Wilcox <github.com@m.cognize.org>
2018-04-08 10:10:52 -0400

    add images uploaded count

--- src/assignments/project-1/components/completedFiles.jsx.js
+++ src/assignments/project-1/components/completedFiles.jsx.js
@@ -3,11 +3,14 @@ import CompletedFilesItem from './completedFilesItem.jsx'
 
 
 const CompletedFiles = ({completedFiles}) => {
-	return <ul>
-			{completedFiles.map( (file) => 
-				<CompletedFilesItem key={file.id} file={file} />
-			)}
-		</ul>
+	return <div>
+				<div className="images-uploaded">Images uploaded: <span>{completedFiles.length}</span></div>
+				<ul>
+					{completedFiles.map( (file) => 
+						<CompletedFilesItem key={file.id} file={file} />
+					)}
+				</ul>
+		</div>
 }
 
 export default CompletedFiles
