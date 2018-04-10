32bd2be28589dd2e02dee4006ea5297bc69d9383
Curtis Wilcox <github.com@m.cognize.org>
2018-04-09 20:02:18 -0400

    change name id to more unique value

--- src/assignments/project-1/components/completedFilesItem.jsx.js
+++ src/assignments/project-1/components/completedFilesItem.jsx.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const CompletedFilesItem = ({file}) => {
-	const {id, name, url, error} = file;
+	const {id, name, url, error, updatedAt} = file;
 
 	return <li className="completedFilesItem">
-				<span className="cf-item-name" id={id}>{name}</span>
-				{!error && <img src={url} aria-labelledby={id} className="cf-item-img" />}
+				<span className="cf-item-name" id={updatedAt}>{name}</span>
+				{!error && <img src={url} aria-labelledby={updatedAt} className="cf-item-img" />}
 				{!!error && <p className="failure">{error}</p>}
 			</li>
 }
