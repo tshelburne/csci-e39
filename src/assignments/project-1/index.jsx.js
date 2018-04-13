a8db7c5c307c73c0c5c05cedcee50f5bbb421193
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2018-04-13 17:08:13 -0400

    added my easter styling :)

--- src/assignments/project-1/index.jsx.js
+++ src/assignments/project-1/index.jsx.js
@@ -1,58 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import { status as statusPropType, file as filePropType } from './scripts/schemas'
+
 import Uploader from '../../ui/components/uploader.jsx'
+import Card from './scripts/components/Card.jsx.js'
 
 const Uploads = ({uploads, actions}) => {
 	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
 	const completedFiles = uploads.files.filter(({progress}) => !progress)
+  completedFiles.__proto__.prune = file => this.splice(this.findIndex(f => f === file), 1)
 
-	return <div>
-		<h1>Upload Images</h1>
-		{/* do not delete this uploader component */}
-		<Uploader upload={actions.upload} />
-		{/* do not delete this uploader component */}
+	return <div className="App mountable component">
+		<h1>The Hunt Is On</h1>
+    <header className="subHead mast">Share FOTOS of where you found your eggs!</header>
+  
+    <section className="content upload">
+  
+      <section className="upload-input">
+        {/* TODO - get FE team to add this label to the uploader */}
+        <label tabIndex="0" htmlFor="upload-1" className="uploader" onClick={clickUpload}>Upload Files		</label>
+        {/* do not delete this uploader component */}
+        <Uploader upload={actions.upload} />
+        {/* do not delete this uploader component */}      
+      </section>
 
-		<h2>In Progress</h2>
-		<ul>
-			{pendingFiles.map(file => {
-				const {id, name, progress} = file
-
-				return <li key={id}>
-					<label>{name}</label>
-					<progress value={progress} max="100">{progress}%</progress>
-				</li>
-			})}
-		</ul>
-
-		<h2>Completed</h2>
-		<ul>
-			{completedFiles.map(file => {
-				const {id, name, url, error} = file
-
-				return <li key={id}>
-					<label>{name}</label>
-					{!error && <img src={url} style={{maxWidth: `200px`}} />}
-					{!!error && <p className="failure">{error}</p>}
-				</li>
-			})}
-		</ul>
+      <ProgressSection pendingFiles={pendingFiles} />
+      
+      <section className="completed">
+        <h2>Completed</h2>
+        <ul className="list--images">
+        	{completedFiles.map(file => <li key={file.id}><Card file={file} remove={file => this.completedFiles.prune(file)} /></li>)}
+        </ul>
+      </section>
+    </section>
 	</div>
 }
 
-const statusPropType = PropTypes.shape({
-	status: PropTypes.oneOf([`init`, `pending`, `success`, `failure`]).isRequired,
-	message: PropTypes.string.isRequired,
-})
-
 Uploads.propTypes = {
 	uploads: PropTypes.shape({
-		files: PropTypes.arrayOf(PropTypes.shape({
-			id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-			name: PropTypes.string.isRequired,
-			progress: PropTypes.number,
-			url: PropTypes.string,
-			error: PropTypes.string,
-		})).isRequired,
+		files: PropTypes.arrayOf(filePropType).isRequired,
 		update: statusPropType.isRequired,
 		delete: statusPropType.isRequired,
 		share: statusPropType.isRequired,
@@ -61,3 +48,39 @@ Uploads.propTypes = {
 }
 
 export default Uploads
+
+// project-1
+
+/** Progress Section component - making it reactive as if Uploads had state */
+const ProgressSection = ({pendingFiles}) => {
+  if(pendingFiles.length) {
+    return <section className="progress">
+  <h2>In Progress</h2>
+  <ul>
+  	{pendingFiles.map(file => {
+  		const {id, name, progress} = file
+
+  		return <li key={id}>
+  			<label>{name}</label>
+  			<progress value={progress} max="100">{progress}%</progress>
+  		</li>
+  	})}
+  </ul>
+</section>
+  } else {
+    return null
+  }
+}
+
+
+/** clickUpload (event) => triggers a click on the corresponding input for a given input label, following the implementation spec:
+    <label for="unique input label" onclick="clickUpload">Unique Input</label>
+    <input type="file" ...>
+
+    TODO - find out how to do this with refs, given we can't change the component
+    TODO - ask FE team to implement the label
+ */
+const clickUpload = (e) => {
+  let selector = e.target.getAttribute('for')
+  document.querySelector(`[for="${selector}"] + input[type="file"]`).click()
+}
---
+++ src/assignments/project-1/scripts/components/Card.jsx.js
@@ -0,0 +1,101 @@
+/** Card component - one for each image */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { file } from '../schemas'
+import { PLACEHOLDER_IMAGE } from '../defs'
+
+class Card extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      imageTitle: '', 
+      editableImageTitle: '',
+      isEditingTitle: true,
+      placeholderImg: PLACEHOLDER_IMAGE
+    }
+    this.refs = {}
+  }
+  
+  componentDidMount () {
+    console.log(this.refs)
+  }
+  
+  render() {
+    let {id, name, url, error} = this.props.file
+    return (
+<article className="Card" ref="card">
+	{error 
+    ? (
+  <figure>  
+    <button className="dismiss" onClick={() => {}}>❌&nbsp;Dismiss</button>  
+      <figcaption title={error}>
+      <span className="filename failure">{error}</span>
+    </figcaption>
+    <img src={this.state.placeholderImg} />
+  </figure>
+      ) 
+    : (
+  <figure>  
+    <span className="control--title">
+      {(!this.state.imageTitle || this.state.isEditingTitle)
+        ? <input type="text" 
+                 className="title input--title" 
+                 placeholder="title" 
+                 value={this.state.editableImageTitle}
+                 onChange={e => this.setState({editableImageTitle: e.target.value})}
+                 onKeyPress={ev => {
+                  if (ev.key === 'Enter') {
+                    // Do code here
+                    this.updateImageTitle(this.state.editableImageTitle)
+                    ev.preventDefault();
+                  }
+                 }}
+                 onBlur={(e) => this.updateImageTitle(e.target.value)}/>
+        : <span className="title" title={this.state.imageTitle} onClick={() => this.setState({isEditingTitle: true})}>{this.state.imageTitle}</span>
+      }
+    </span>
+    {(this.state.editableImageTitle !== this.state.imageTitle)
+      ? (
+    <button className="changeImageTitle" onClick={() => this.updateImageTitle(this.state.editableImageTitle)}>
+        🆙 Submit
+    </button>
+        )
+      : ''
+    }
+    <figcaption title={name}>
+      <span className="filename">{name}</span>
+    </figcaption>
+    <img className="cardImage" src={url} />
+  </figure>
+      )
+    }
+</article>  
+    )
+  } 
+  
+  /** updates the imageTitle state */
+  updateImageTitle (title) {
+    if(title) {
+      this.setState({isEditingTitle: false, imageTitle: title, editableImageTitle: title})      
+    } else {
+      this.setState({isEditingTitle: false, imageTitle: '', editableImageTitle: ''})
+    }
+  }
+  
+  isTruncated (ref) {
+    if(!this.refs[ref])
+      return
+
+    let el = this.refs[ref]
+    
+    return (el.offsetWidth < el.scrollWidth)
+  }
+}
+
+Card.propTypes = {
+  file, 
+  remove: PropTypes.func
+}
+
+export default Card
