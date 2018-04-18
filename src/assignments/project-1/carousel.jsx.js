f2ec04fa633a504a8ecc0893ef5a955c3970bf53
Muhammad Abdurrahman <protik@live.com>
2018-04-19 01:44:00 +1000

    updated carousel images

--- src/assignments/project-1/carousel.jsx.js
+++ src/assignments/project-1/carousel.jsx.js
@@ -1,6 +1,13 @@
 /* Banner carousel */
 /* Forked from https://github.com/akiran/react-slick */
 
+//1-1170: https://cdn.pbrd.co/images/Hhcxv3j.jpg 
+//1-450: https://cdn.pbrd.co/images/HhcxXlT.jpg
+
+//2-1170: https://cdn.pbrd.co/images/Hhcyi7E.jpg 
+//2-450: https://cdn.pbrd.co/images/HhcyxFW.jpg 
+
+
 import React from 'react'
 import Slider from 'react-slick'
 
@@ -17,13 +24,13 @@ class Carousel extends React.Component {
       <Slider {...settings}>
         <picture>
            <source media="(max-width: 767px)" srcSet="https://cdn.pbrd.co/images/Hh2pCmQ.jpg" />
-           <source media="(min-width: 768px)" srcSet="https://cdn.pbrd.co/images/Hh2pr1X.jpg" />
-           <img src="https://cdn.pbrd.co/images/Hh2pr1X.jpg" title ="Wakhan Valley" alt="Wakhan Valley at Tajikistan/Afganistan border" />
+           <source media="(min-width: 768px)" srcSet="https://cdn.pbrd.co/images/Hhcxv3j.jpg " />
+           <img src="https://cdn.pbrd.co/images/Hhcxv3j.jpg " title ="Wakhan Valley" alt="Wakhan Valley at Tajikistan/Afganistan border" />
         </picture>
         <picture>
            <source media="(max-width: 767px)" srcSet="https://cdn.pbrd.co/images/Hh2cnaR.jpg" />
-           <source media="(min-width: 768px)" srcSet="https://cdn.pbrd.co/images/Hh2ch00.jpg" />
-           <img src="https://cdn.pbrd.co/images/Hh2ch00.jpg" title ="Mt Kosciuszko" alt="Mount Kosciuszko is the highest mountain in Australia" />
+           <source media="(min-width: 768px)" srcSet="https://cdn.pbrd.co/images/Hhcyi7E.jpg" />
+           <img src="https://cdn.pbrd.co/images/Hhcyi7E.jpg" title ="Mt Kosciuszko" alt="Mount Kosciuszko is the highest mountain in Australia" />
         </picture>
       </Slider>
     );
