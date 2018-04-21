714bc374651d62b051f112309776c33d05b14144
pingji00 <pji@wellesley.edu>
2018-04-21 09:25:24 -0400

    change view.jsx

--- src/assignments/project-1/components/View.jsx.js
+++ src/assignments/project-1/components/View.jsx.js
@@ -6,7 +6,29 @@ function ChangeView () {
 }
 
 export class ViewChange extends React.Component {
-		render() {
+	// constructor () {
+	// 	this.state = { grid: false }
+	// }
+	// setState (state) {
+	// 	this.state = state
+	// 	this.el = this.render()
+	// }
+	// changeViewText () {
+	// 	this.setState({
+	// 		grid: !this.state.grid
+	// 	})
+	// }
+	// render () {
+	// 	this.el = createDOMFromString(` 
+	// 		<button class='view'>
+	// 		<span class='like-text'>${this.state.grid ? 'grid' : 'line'}</span>
+	// 		<span><!-- error --></span>
+	// 		</button>
+	// 		`)
+	// 	this.el.addEventListener('click', this.changeViewText.bind(this), false)
+	// 	return this.el
+	// }
+	render() {
 			return <button className="view" onClick={ChangeView}>{this.props.text}</button>;
 		}
-	}
+}
