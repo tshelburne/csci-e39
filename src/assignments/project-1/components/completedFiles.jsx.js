e0ecf1fea77d7adbace78ac1359d9e314d1e362e
Curtis Wilcox <github.com@m.cognize.org>
2018-04-21 00:23:05 -0400

    style images uploaded

--- src/assignments/project-1/components/completedFiles.jsx.js
+++ src/assignments/project-1/components/completedFiles.jsx.js
@@ -4,7 +4,7 @@ import CompletedFilesItem from './completedFilesItem.jsx'
 
 const CompletedFiles = ({completedFiles}) => {
 	return <div className="completedFiles">
-				<div className="images-uploaded">Images uploaded: <span>{completedFiles.length}</span></div>
+				<div className="images-uploaded">Images uploaded: <span className="completedFiles-value">{completedFiles.length}</span></div>
 				<ul className="cf-ul">
 					{completedFiles.map( (file) => 
 						<CompletedFilesItem key={file.id} file={file} />
