1c5c13382b300a6bec148047207dc2c3c2464c7a
Curtis Wilcox <github.com@m.cognize.org>
2018-04-23 23:49:39 -0400

    add aria-hidden toggle to pendingFiles

--- src/assignments/project-1/components/pendingFiles.jsx.js
+++ src/assignments/project-1/components/pendingFiles.jsx.js
@@ -20,22 +20,29 @@ import PropTypes from 'prop-types'
 class PendingFiles extends React.Component {
 
 	constructor() {
-		super(...arguments)
+		super(...arguments);
+		this.state = {hidden: true};
 	}
 
 	reveal() {
 		const {pendingFiles} = this.props;
 		const checkLength = pendingFiles.length;
 		if(checkLength === 0) {
+			if (this.state.hidden === false) {
+				this.setState({hidden: true});
+			}
 			return 'sr-only hide'
 		}
 		else {
+			if (this.state.hidden === true) {
+				this.setState({hidden: false});
+			}
 			return null
 		}
 	}
 
 	render() {
-		return <div className={['pendingFiles', this.reveal()].join(' ')}>
+		return <div className={['pendingFiles', this.reveal()].join(' ')} aria-hidden={this.state.hidden}>
 		<h2>In Progress</h2>
 		<ul>
 			{this.props.pendingFiles.map(file => {
