7d0a2a694ba126040db011321f698b2cf03f643d
Michael Regert <mjr171@g.harvard.edu>
2018-04-25 17:10:21 -0500

    Fixed functionality, now back to layout and CSS

--- src/assignments/project-1/index.jsx.js
+++ src/assignments/project-1/index.jsx.js
@@ -22,10 +22,6 @@ class Uploads extends React.Component {
 		this.setState( {uploaderActive: !this.state.uploaderActive} )
 	}
 
-	onNavChange() {
-		console.log("Page = ")
-	}
-
 	render() {
 		const {uploads, actions} = this.props
 		const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
@@ -44,69 +40,80 @@ class Uploads extends React.Component {
 
 				<nav className="mainNavigation" role="navigation">
 					<ul>
-						<li><a href='javascript:;' onClick={this.onNavChange().bind(this)}>Home</a></li>
+						<li><a href='javascript:void(0)' onClick={ () => this.setState( {currentPage: 'home'})}>HOME</a></li>
+						<li><a href='javascript:void(0)' onClick={ () => this.setState( {currentPage: 'photo'})}>PHOTO</a></li>
+						<li><a href='javascript:void(0)' onClick={ () => this.setState( {currentPage: 'faq'})}>FAQ</a></li>
 			  		</ul>
 				</nav>
 
-				<div className="uploader">
-					<h2>Upload Image</h2>
-					<Uploader upload={actions.upload} />
-					<List>
-						{pendingFiles.map((file, index) =>
-							<PendingFile
-								key={file.id}
-								file={file}
-							/>
-						)}
-					</List>
-				</div>
-
-				<Thumbnails className="thumbnails" completedFiles={completedFiles} />
-
 				<main className="large-photo">
-					{ currentPage=="faq" &&
-					<div>
-						<h1>FAQ</h1>
-						<p>
-						This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
-						we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
-						</p>
-						<p>
-						This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
-						exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
-						about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
-						</p>
-						<p>
-						This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
-						we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
-						</p>
-						<p>
-						This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
-						exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
-						about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
-						</p>
-						<p>
-						This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
-						we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
-						</p>
-						<p>
-						This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
-						exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
-						about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
-						</p>
-						<p>
-						This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
-						we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
-						</p>
-						<p>
-						This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
-						exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
-						about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
-						</p>
-					</div>
-					}
 					{ currentPage=="home" &&
-						<h1>Large Photo Here</h1>
+						<div>
+							<h1>CSCI E-39</h1>
+							<h2>Modular Design Patterns with React</h2>
+							<h3>Michael Regert</h3>
+							<br />
+							<a href='javascript:void(0)' onClick={ () => this.setState( {currentPage: 'photo'})}>Go to the Photo Viewer</a>
+						</div>
+					}
+					{ currentPage=="photo" &&
+						<div>
+							<h1>Large Photo Here</h1>
+							<div className="uploader">
+								<h2>Upload Image</h2>
+								<Uploader upload={actions.upload} />
+								<List>
+									{pendingFiles.map((file, index) =>
+										<PendingFile
+											key={file.id}
+											file={file}
+										/>
+									)}
+								</List>
+							</div>
+							<Thumbnails className="thumbnails" completedFiles={completedFiles} />
+						</div>
+					}
+					{ currentPage=="faq" &&
+						<div>
+							<h1>FAQ</h1>
+							<p>
+							This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
+							we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
+							</p>
+							<p>
+							This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
+							exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
+							about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
+							</p>
+							<p>
+							This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
+							we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
+							</p>
+							<p>
+							This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
+							exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
+							about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
+							</p>
+							<p>
+							This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
+							we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
+							</p>
+							<p>
+							This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
+							exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
+							about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
+							</p>
+							<p>
+							This is a paragraph! What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard
+							we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel. -
+							</p>
+							<p>
+							This is another paragraph. Are you all right? What's wrong? I felt a great disturbance in the Force...as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened. You'd better get on with your
+							exercises. Well, you can forget your troubles with those Imperial slugs. I told you I'd outrun 'em. Don't everyone thank me at once. Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Artoo. He made a fair move. Screaming
+							about it won't help you. Let him have it. It's not wise to upset a Wookiee. But sir, nobody worries about upsetting a droid. That's 'cause droids don't pull people's arms out of their socket when they lose. -
+							</p>
+						</div>
 					}
 				</main>
 
