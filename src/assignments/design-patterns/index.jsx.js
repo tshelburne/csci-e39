85aa6144a0994e730440b0224c750256b8856372
Tim Shelburne <shelburt02@gmail.com>
2018-04-25 16:22:56 -0400

    created styleguide index with base (read: garbage) styles

---
+++ src/assignments/design-patterns/index.jsx.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Example from './support/example.jsx'
+
+class PatternLibrary extends React.Component {
+
+	constructor(...args) {
+		super(...args)
+
+		this.state = {activeCode: `react`}
+	}
+
+	getChildContext() {
+		return {
+			activeCode: this.state.activeCode,
+			setActiveCode: activeCode => this.setState({activeCode}),
+		}
+	}
+
+	render() {
+		return (
+			<div className="style-guide">
+				<h1>My Pattern Library!</h1>
+
+				<Example title="My Special <div>">
+					<div className="just-testing">HELLO DIV</div>
+				</Example>
+				
+				<Example title="My Special <span>">
+					<span className="just-testing">HELLO SPAN</span>
+				</Example>
+
+				<Example title="My Special <h4>">
+					<h4 className="just-testing">HELLO HEADING</h4>
+				</Example>
+			</div>
+		)
+	}
+}
+
+PatternLibrary.childContextTypes = {
+	activeCode: PropTypes.string,
+	setActiveCode: PropTypes.func,
+}
+
+export default PatternLibrary
