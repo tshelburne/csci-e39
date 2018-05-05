28fc96729fe3d3811dea7260919aa10af3f4c6a4
Curtis Wilcox <github.com@m.cognize.org>
2018-05-05 16:03:39 -0400

    change error example time to be unique

--- src/assignments/design-patterns/components/completedFilesItem.jsx.js
+++ src/assignments/design-patterns/components/completedFilesItem.jsx.js
@@ -24,7 +24,7 @@ export const CompletedFilesItemExamples = {
 			name:"example image", 
 			url: "https://avatars0.githubusercontent.com/u/6720549?s=200&v=4", 
 			error: "Max file size 500KB", 
-			updatedAt: "1525968000000",
+			updatedAt: "1525968000001",
 		},
 };
 
