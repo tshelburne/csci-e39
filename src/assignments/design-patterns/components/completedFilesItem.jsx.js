349a133ee17504c9e7791ff93088a4c5ab4c857d
Curtis Wilcox <github.com@m.cognize.org>
2018-05-05 15:46:11 -0400

    add example props to CompletedFilesItem

--- src/assignments/design-patterns/components/completedFilesItem.jsx.js
+++ src/assignments/design-patterns/components/completedFilesItem.jsx.js
@@ -10,4 +10,22 @@ const CompletedFilesItem = ({file}) => {
 			</li>
 }
 
+export const CompletedFilesItemExamples = {
+		"one": {
+			id:"1", 
+			name:"example image", 
+			url: "https://avatars0.githubusercontent.com/u/6720549?s=200&v=4", 
+			error: "", 
+			updatedAt: "1525968000000",
+		},
+
+		"error": {
+			id:"1", 
+			name:"example image", 
+			url: "https://avatars0.githubusercontent.com/u/6720549?s=200&v=4", 
+			error: "Max file size 500KB", 
+			updatedAt: "1525968000000",
+		},
+};
+
 export default CompletedFilesItem
--- src/assignments/design-patterns/index.jsx.js
+++ src/assignments/design-patterns/index.jsx.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Example from './support/example.jsx'
-import CompletedFilesItem from './components/completedFilesItem.jsx.js'
+import CompletedFilesItem, {CompletedFilesItemExamples} from './components/completedFilesItem.jsx.js'
 
 class PatternLibrary extends React.Component {
 
@@ -19,14 +19,6 @@ class PatternLibrary extends React.Component {
 	}
 
 	render() {
-		const CompletedFilesItemFile = {
-			id:"1", 
-			name:"example image", 
-			url: "https://avatars0.githubusercontent.com/u/6720549?s=200&v=4", 
-			error: "", 
-			updatedAt: "1525968000000",
-		}
-
 		return (
 			<div className="style-guide">
 				<h1>My Pattern Library!</h1>
@@ -36,11 +28,11 @@ class PatternLibrary extends React.Component {
 				</Example>
 				
 				<Example title="CompletedFilesItem">
-					<CompletedFilesItem file={CompletedFilesItemFile} />
+					<CompletedFilesItem file={CompletedFilesItemExamples.one} />
 				</Example>
 
-				<Example title="My Special <h4>">
-					<h4 className="just-testing">HELLO HEADING</h4>
+				<Example title="CompletedFilesItem Error">
+					<CompletedFilesItem file={CompletedFilesItemExamples.error} />
 				</Example>
 			</div>
 		)
