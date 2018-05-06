fd42717e8874c0c7ef90de4e29dafbc89f277248
Annette Lin <al261310@gmail.com>
2018-05-06 15:55:19 -0400

    clean up files

--- src/assignments/design-patterns/navigation.jsx.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Navigation = ({text, styleClass, href}) =>
-	<a className = {'${styleClass}'}  href = "{href}">{text}/a>
-
-export const linkOne = ({text, href}) =>
-	<a href = "{href}" styleClass = "link1">{text}</a>
-
