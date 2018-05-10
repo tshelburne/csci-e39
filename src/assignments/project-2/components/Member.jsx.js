58341c09b4287a18b7f2fc1c73e329e0b91d1cec
David Longo <longodj@gmail.com>
2018-05-10 03:54:47 -0400

    Remember that time we had to fix Project 2 to get this to build? 😂

---
+++ src/assignments/project-2/components/Member.jsx.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from '../support/prop-types'
+
+class Member extends Component {
+
+    render() {
+        const { id, name } = this.props
+        return (
+            <div>{id} - {name}</div>
+        )
+
+    }
+}
+
+Member.propType = {
+    member: PropTypes.member.isRequired,
+}
+
+export default Member
