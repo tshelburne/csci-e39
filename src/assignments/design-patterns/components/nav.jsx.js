0cada9e08d6def6b9dc9036462704637b7f918ba
Luann Ebert <lu@flyingdog.nu>
2018-05-10 12:08:27 -0400

    added hamburger compoent, reorganized diretory structure

---
+++ src/assignments/design-patterns/components/nav.jsx.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Hamburger = (  ) => 
+    <div id="hamburger-container" className="transform">
+      <div className="x-forward transform"></div>
+      <div className="x-back transform"></div>
+    </div>
+
