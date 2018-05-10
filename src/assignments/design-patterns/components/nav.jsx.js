c54482d50349337de3a9464e2f13c5ba0e997b49
Luann Ebert <lu@flyingdog.nu>
2018-05-10 12:36:42 -0400

    clean up directory structure

--- src/assignments/design-patterns/components/nav.jsx.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Hamburger = (  ) => 
-    <div id="hamburger-container" className="transform">
-      <div className="x-forward transform"></div>
-      <div className="x-back transform"></div>
-    </div>
-
