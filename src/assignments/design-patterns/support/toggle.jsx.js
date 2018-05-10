6aabd40dd63feb367bafbb0b367a6fb147997827
Sean Beck <spcbeck@gmail.com>
2018-05-09 20:10:58 -0400

    Toggle now accepts an array of options, and can set a default selected option. Styles mostly complete.

--- src/assignments/design-patterns/support/toggle.jsx.js
+++ src/assignments/design-patterns/support/toggle.jsx.js
@@ -2,27 +2,43 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Toggle extends Component {
+	constructor(...args) {
+		super(...args)
+		this.state = {
+			selectedOption: this.props.selectedOption
+		}
+	}
+
 	render() {
 	    const {
-	      model,
+	    	name,
+	      options,
 	      color
 	    } = this.props
-	    return ( 
-	      <div className="toggle">
-	      	<div className="toggle-option">
-	      		<input type="radio" />
-	      	</div>
-	      </div>
-	    )
+
+	    return  <div className="toggle">
+		    	{ options.map((option, i) => {
+				      return <label className="toggle-option">
+					      		<input name={name} type="radio" defaultChecked={ this.state.selectedOption === option } />
+					      		<span>{option}</span>
+					      	</label>
+			  		})
+			  	}
+	  		</div>
 	}
 }
 
+
 Toggle.propTypes = {
-    color: string,
-    options: PropTypes.object
+	name: PropTypes.string,
+    color: PropTypes.oneOf(['white', 'black', 'grey']),
+    options: PropTypes.array,
+    selectedOption: PropTypes.string
 }
 
 Toggle.defaultProps = {
+	name: 'radios',
     color: 'white',
-    amount: 2
+   	options: ['A', 'B'],
+   	selectedOption: 'A'
 }
