961ae2054fb6785f7cbc8be152df71304b32dbb4
James Massa <james.l.massa@gmail.com>
2019-03-31 11:51:46 -0400

    Added responsive css grid and styled the images

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -4,14 +4,23 @@ import Uploader from '../../ui/components/uploader'
 
 import './app.scss'
 
+
 const Uploads = ({uploads, actions}) => {
 	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
 	const completedFiles = uploads.files.filter(({progress}) => !progress)
 
-	return <div>
+	return <div className="example-container">
+	  <header className="hero">
+		<h1>Birthday Album</h1>
+			<p> Welcome to my birthday party album.  This is me with my wife, Natasha and our two
+			    children, Zayden and Violet.  We went to <a href="https://www.nickandtonis.com/">Nick and Toni's </a>
+				restaurant in East Hampton.
+				Dinner was good enough but the Tartufo desert was to die for--best we have ever eaten.
+				Next time, we will skip dinner and just eat tartufo.
+			</p>
 		<h1>Upload Images</h1>
 		{/* do not delete this uploader component */}
-		<Uploader upload={actions.upload} />
+		<Uploader className="uploader-input" upload={actions.upload} />
 		{/* do not delete this uploader component */}
 
 		<h2>In Progress</h2>
@@ -21,23 +30,32 @@ const Uploads = ({uploads, actions}) => {
 
 				return <li key={id}>
 					<label>{name}</label>
-					<progress value={progress} max="100">{progress}%</progress>
+					<progress value={progress} value = "33" max="100">{progress}%</progress>
 				</li>
 			})}
 		</ul>
 
 		<h2>Completed</h2>
-		<ul>
-			{completedFiles.map(file => {
-				const {id, name, url, error} = file
-
-				return <li key={id}>
-					<label>{name}</label>
-					{!error && <img src={url} style={{maxWidth: `200px`}} />}
-					{!!error && <p className="failure">{error}</p>}
-				</li>
-			})}
-		</ul>
+	   </header>
+	   
+		<section className="photos"> 
+			<ul className="photo-grid">
+				{completedFiles.map(file => {
+					const {id, name, url, error} = file
+	
+					return <li className="photo-and-label" key={id}>
+						<figure>
+							<figcaption className="filename">{name}</figcaption>
+							{!error && <img src={url} style={{maxWidth: `200px`}} className="photo" />}
+							{!!error && <p className="failure">{error}</p>}
+						</figure>						
+					</li>
+				})}
+			</ul>
+		</section>
+		<footer className="footer">
+			<h2 className="header">This is the footer</h2>
+		</footer>
 	</div>
 }
 
