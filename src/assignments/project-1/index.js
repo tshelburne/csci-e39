53679b7b09a48c004fafa349f1b6c3e70fdf15c7
Zoraida <zmcabrera@gmail.com>
2019-04-04 18:32:18 -0400

    added alt text and created a two column layout

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -4,42 +4,52 @@ import Uploader from '../../ui/components/uploader'
 
 import './app.scss'
 
-const Uploads = ({uploads, actions}) => {
-	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
-	const completedFiles = uploads.files.filter(({progress}) => !progress)
+const Uploads = ({ uploads, actions }) => {
+	const pendingFiles = uploads.files.filter(({ progress }) => progress && progress < 100)
+	const completedFiles = uploads.files.filter(({ progress }) => !progress)
 
-	return <div>
-		<h1>Upload Images</h1>
-		<label tabindex="0" for="uploader" class="uploader">Upload Photos</label>
-		{/* do not delete this uploader component */}
-		<Uploader id="uploader" className="uploader-input" upload={actions.upload} />
-		{/* do not delete this uploader component */}
+	return <React.Fragment>
+		<header>
+			<h1>Thinking of California</h1>
+		</header>
+		<main className="wrapper">
+			<div class="sidebar">
+			<h2>Photo Uplaoder</h2>
+				<label for="uploader" class="uploader">Add Photos</label>
+				{/* do not delete this uploader component */}
+				<Uploader id="uploader" className="uploader-input" upload={actions.upload} />
+				{/* do not delete this uploader component */}
+				
+				<h3>Photos In Progress</h3>
+				<ul className="in-progress-imgs">
+				{console.log(uploads.files.progress)}
+					{pendingFiles.map(file => {
+						const { id, name, progress } = file
+						return <li key={id}>
+							<label>{name}</label>
+							<progress value={progress} max="100">{progress}%</progress>
+						</li>
+					})}
+				</ul>
+			</div>
 
-		<h2>In Progress</h2>
-		<ul>
-			{pendingFiles.map(file => {
-				const {id, name, progress} = file
+			<div className="album-container">
+				<h2>Photo Album</h2>
+				<ul className="completed-imgs">
+					{completedFiles.map(file => {
+						const { id, name, url, error } = file
+						const labelName = name.substr(0, name.lastIndexOf('.')) || name; //remove file extension
 
-				return <li key={id}>
-					<label>{name}</label>
-					<progress value={progress} max="100">{progress}%</progress>
-				</li>
-			})}
-		</ul>
-
-		<h2>Completed</h2>
-		<ul>
-			{completedFiles.map(file => {
-				const {id, name, url, error} = file
-
-				return <li key={id}>
-					{!error && <img src={url} style={{maxWidth: `200px`}} />}
-					{!!error && <p className="failure">{error}</p>}
-					<label>{name}</label>
-				</li>
-			})}
-		</ul>
-	</div>
+						return <li key={id}>
+							{!error && <img id={id} src={url} style={{ maxWidth: `200px` }} alt={labelName} />}
+							{!!error && <p className="failure">{error}</p>}
+							<label>{labelName}</label>
+						</li>
+					})}
+				</ul>
+			</div>
+		</main>
+	</React.Fragment>
 }
 
 const statusPropType = PropTypes.shape({
