9135846469fe9be573037ececa25a68062730a7f
Zoraida <zmcabrera@gmail.com>
2019-04-05 12:53:11 -0400

    changed layout for page and grid along with font styles and colors

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -7,58 +7,62 @@ import './app.scss'
 
 const Uploads = ({ uploads, actions }) => {
 	const pendingFiles = uploads.files.filter(({ progress }) => progress && progress < 100)
-	const completedFiles = uploads.files.filter(({ progress }) => !progress)
 	const pendingFilesTotal = pendingFiles.length;
+	const completedFiles = uploads.files.filter(({ progress }) => !progress)
+
 	return <React.Fragment>
-
-		<main className="wrapper">
-			<div className="album-container">
+		<div className="wrapper">
 			<header>
-			<h1>California Days</h1>
+				<h1>California Days</h1>
 			</header>
-			
-				<ul className="completed-imgs">
-					{completedFiles.map(file => {
-						const { id, name, url, error } = file
-						const labelName = name.substr(0, name.lastIndexOf('.')) || name; //remove file extension
 
-						return <li key={id}>
-							{!error && <img id={id} src={url} alt={labelName} />}
-							{!!error && <p className="failure">{error}</p>}
-							<label>{labelName}</label>
-							{/* YOU WILL NEED TO CREATE A CLASS COMPONENT DIFFERENT THAN UPLOAD WITH slideIMG}
-							{/* <button className="button" onClick={()=> this.setState({slideImg: file})}>See Detail</button> */} 
-						
-						</li>
-					})
-					}
-				</ul>
-			</div>
-
-			<div class="sidebar">
-			<h2>Photo Uplaoder</h2>
-				<label for="uploader" class="uploader">Add Photos</label>
-				{/* do not delete this uploader component */}
-				<Uploader id="uploader" className="uploader-input" upload={actions.upload} />
-				{/* do not delete this uploader component */}
+			<main>
 				
-				{pendingFilesTotal > 0 && <h3>Photos In Progress</h3>} 
-				<ul className="in-progress-imgs">
-					{pendingFiles.map(file => {
-						const { id, name, progress } = file
-						return <li key={id}>
-							<label>{name}</label>
-							<progress value={progress} max="100">{progress}%</progress>
-						</li>
-					})}
-				</ul>
-
-				<h2>Have Questions?</h2>
-				<a href="/#/faq.html" className="button">Checkout My FAQs</a>
-			</div>
+					<label for="uploader" class="uploader"><i class="fas fa-camera-retro" aria-hidden="true"></i> Add Photos</label>
+					{/* do not delete this uploader component */}
+					<Uploader id="uploader" className="uploader-input" upload={actions.upload} />
+					{/* do not delete this uploader component */}
 
 
-		</main>
+					{pendingFilesTotal > 0 && <h3>Photos In Progress</h3>}
+					<ul className="in-progress-imgs">
+						{pendingFiles.map(file => {
+							const { id, name, progress } = file
+							return <li key={id}>
+								<label>{name}</label>
+								<progress value={progress} max="100">{progress}%</progress>
+							</li>
+						})}
+					</ul>
+						
+					<nav>
+					<ul className="tabs">
+						<li><button className="active"><i class="fas fa-th" aria-hidden="true"></i> Album</button></li>
+						<li><button><i class="fas fa-question" aria-hidden="true"></i> FAQ</button></li>
+					</ul>
+					</nav>
+					
+
+				<div className="album-container">
+					<ul className="completed-imgs">
+						{completedFiles.map(file => {
+							const { id, name, url, error } = file
+							const labelName = name.substr(0, name.lastIndexOf('.')) || name; //remove file extension
+
+							return <li key={id}>
+								{!error && <img id={id} src={url} alt={labelName} />}
+								{!!error && <p className="failure">{error}</p>}
+								<label>{labelName}</label>
+							</li>
+						})
+						}
+					</ul>
+				</div>
+
+
+
+			</main>
+		</div>
 	</React.Fragment>
 }
 
