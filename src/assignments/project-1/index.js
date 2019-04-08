97816326ea0c77fb0b872ccc6c380b00cb54744b
James Massa <james.l.massa@gmail.com>
2019-04-08 08:04:48 -0400

    Added CSS reset and color scheme

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -6,12 +6,6 @@ import './app.scss'
 
 /* COMPONENTS */
 
-function Footer(props) {
-
-  return 		<footer className="footer">
-					<h2>{props.footerText}</h2>
-				</footer>;
-}
 
 function Photo(props) {
 
@@ -24,41 +18,52 @@ function Photo(props) {
 					</li>;
 }
 
+function Footer(props) {
+
+  return 		<footer className="footer">
+					<h2>{props.footerText}</h2>
+				</footer>;
+}
+
+
 const Uploads = ({uploads, actions}) => {
 	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
 	const completedFiles = uploads.files.filter(({progress}) => !progress)
 
 	return <div className="example-container">
-	  <header className="hero">
-		<h1>Birthday Album</h1>
-			<p> Welcome to my birthday party album.  This is me with my wife, Natasha, and our two
-			    children, Zayden and Violet.  We went to <a href="https://www.nickandtonis.com/">Nick and Toni's </a>
-				restaurant in East Hampton.
-				Dinner was good enough but the Tartufo desert was to die for--best we have ever eaten.
-				Next time, we will skip dinner and just eat tartufo!
-			</p>
-		
-		{/* do not delete this uploader component */}
-		<button className="uploader-container">
-			Upload a Photo to the Album
-			<Uploader className="uploader-input" upload={actions.upload} />
-		</button>
-		{/* do not delete this uploader component */}
-
-		<h2>In Progress</h2>
-		<ul>
-			{pendingFiles.map(file => {
-				const {id, name, progress} = file
-
-				return <li key={id}>
-					<label>{name}</label>
-					<progress value={progress} value = "33" max="100">{progress}%</progress>
-				</li>
-			})}
-		</ul>
-
-		<h2>Completed</h2>
-	   </header>
+	    
+		<header className="hero">
+			<h1>Birthday Album</h1>
+				<p> Welcome to my birthday party album.  This is me with my wife, Natasha, and our two
+					children, Zayden and Violet.  We went to <a href="https://www.nickandtonis.com/">Nick and Toni's </a>
+					restaurant in East Hampton.
+					Dinner was good enough but the Tartufo desert was to die for--best we have ever eaten.
+					Next time, we will skip dinner and just eat tartufo!
+				</p>
+		</header>
+	  	
+		<section className="uploader-section">
+			{/* do not delete this uploader component */}
+			<button className="uploader-container">
+				Upload a Photo to the Album
+				<Uploader className="uploader-input" upload={actions.upload} />
+			</button>
+			{/* do not delete this uploader component */}
+	
+			<h2>In Progress</h2>
+			<ul>
+				{pendingFiles.map(file => {
+					const {id, name, progress} = file
+	
+					return <li key={id}>
+						<label>{name}</label>
+						<progress value={progress} value = "33" max="100">{progress}%</progress>
+					</li>
+				})}
+			</ul>
+	
+			<h2>Completed</h2>
+	    </section>
 	   
 		<section className="photos"> 
 			<ul className="photo-grid">
