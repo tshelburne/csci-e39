ceea251d1b3867394c3ed866f9b9f5dd5e8e826d
mvartani76 <mike.vartanian@gmail.com>
2019-04-09 11:17:14 -0400

    Initial attempt at adding description to alt field and and updater component.

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Uploader from '../../ui/components/uploader'
 import SectionHeader from './sectionheader'
+import Updater from './updater'
 
 import './app.scss'
 
@@ -16,6 +17,8 @@ const Uploads = ({uploads, actions}) => {
 		{/* do not delete this uploader component */}
 		<Uploader className="file-form" upload={actions.upload} />
 		{/* do not delete this uploader component */}
+
+
 
 		<SectionHeader tag="h2" sectiontext="In Progress"/>
 		<ul className = "grid-container">
@@ -32,11 +35,12 @@ const Uploads = ({uploads, actions}) => {
 		<SectionHeader tag="h2" sectiontext="Completed"/>
 		<ul className = "grid-container">
 			{completedFiles.map(file => {
-				const {id, name, url, error} = file
+				const {id, name, url, description, error} = file
 
 				return <li className = "card" key={id}>
 					<label className = "card-title">{name}</label>
-					{!error && <img className = "card-img" src={url} />}
+					{!error && <img className = "card-img" src={url} alt={description} />}
+					{!error && <Updater className="form-input" file={file} updateFile={actions.files.updateFile}/>}
 					{!!error && <p className="failure">{error}</p>}
 				</li>
 			})}
@@ -56,6 +60,7 @@ Uploads.propTypes = {
 			name: PropTypes.string.isRequired,
 			progress: PropTypes.number,
 			url: PropTypes.string,
+			description: PropTypes.string,
 			error: PropTypes.string,
 		})).isRequired,
 		update: statusPropType.isRequired,
