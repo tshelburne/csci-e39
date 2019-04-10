966b75561ac4a7a899b265b4dcbb8bbdc376a808
James Massa <james.l.massa@gmail.com>
2019-04-09 23:52:00 -0400

    Cleaned up pending and completed display

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Uploader from '../../ui/components/uploader'
+import Deleter from '../../ui/components/deleter'
 
 import './app.scss'
 
@@ -9,7 +10,8 @@ import './app.scss'
 
 function Header(props) {
 
-  return 		<header className="hero">
+  return 	(
+				<header className="hero">
 					<h2>{props.headerText}</h2>
 										
 					<p> Welcome to my birthday party album.  This is me with my wife, Natasha, and our two
@@ -19,25 +21,30 @@ function Header(props) {
 						Next time, we will skip dinner and just eat tartufo!
 					</p>
 
-				</header>;
+				</header>
+			);
 }
 
 function Photo(props) {
 
-  return 		<li className="photo-and-label" key={props.id}>
-						<figure>
-							<figcaption className="filename">{props.name}</figcaption>
-							{!props.error && <img src={props.url} style={{maxWidth: `200px`}} className="photo" />}
-							{!!props.error && <p className="failure">{error}</p>}
-						</figure>						
-					</li>;
+  return 	(
+				<li className="photo-and-label" key={props.id}>
+					<figure>
+						<figcaption className="filename">{props.name}</figcaption>
+						{!props.error && <img src={props.url} style={{maxWidth: `200px`}} className="photo" />}
+						{!!props.error && <p className="failure">{error}</p>}
+					</figure>						
+				</li>
+			);
 }
 
 function Footer(props) {
 
-  return 		<footer className="footer">
+  return 	(
+				<footer className="footer">
 					<h2>{props.footerText}</h2>
-				</footer>;
+				</footer>
+			);
 }
 
 
@@ -49,7 +56,7 @@ const Uploads = ({uploads, actions}) => {
 	    
 		<Header headerText="Birthday Album" />
 	  	
-		<section className="uploader-section">
+		<section className="control-section">
 			{/* do not delete this uploader component */}
 			<button className="uploader-container">
 				Upload a Photo
@@ -57,11 +64,12 @@ const Uploads = ({uploads, actions}) => {
 			</button>
 			{/* do not delete this uploader component */}
 	
-			<h2>In Progress</h2>
+			
 			<ul>
 				{pendingFiles.map(file => {
 					const {id, name, progress} = file
 	
+					return <h2> Uploading {pendingFiles.length}) Files </h2>
 					return <li key={id}>
 						<label>{name}</label>
 						<progress value={progress} value = "33" max="100">{progress}%</progress>
@@ -69,7 +77,8 @@ const Uploads = ({uploads, actions}) => {
 				})}
 			</ul>
 	
-			<h2>Completed</h2>
+			<h2> {completedFiles.length} Files in Album</h2>
+	
 	    </section>
 	   
 		<section className="photos"> 
