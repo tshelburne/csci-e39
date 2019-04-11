59deb91a52211298bd9e16eaa716cf379d708700
terichadbourne <terichad@gmail.com>
2019-04-10 23:57:41 -0400

    Add favorite state and button

--- src/assignments/project-1/components/card.js
+++ src/assignments/project-1/components/card.js
@@ -7,13 +7,26 @@ class Card extends React.Component {
 	constructor() {
 		super(...arguments)
 		autobind(this)
+		this.state = {
+			isFavorite: false
+		}
+		// This binding is necessary to make `this` work in the callback
+		this.toggleFavorite = this.toggleFavorite.bind(this);
 	}
+
+	toggleFavorite() {
+     this.setState(state => ({
+       isFavorite: !state.isFavorite
+
+     }));
+  }
 
 	render() {
 		const { id, name, url, error} = this.props
 		return <li key={id} className="card">
 			{!error && <img src={url} style={{maxWidth: `200px`}} />}
 			<h2>{name}</h2>
+			<button onClick={this.toggleFavorite} className="favorite-button" aria-label={this.state.isFavorite ? "Remove from favorites" : "Add to favorites"} data-favorite={this.state.isFavorite} > &#10084; </button>
 		</li>
 	}
 }
