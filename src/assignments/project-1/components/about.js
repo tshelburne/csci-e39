543b20060c117341ca941f3aee162d7c2ea3363e
Emily Bottis <ebottis4@gmail.com>
2019-04-12 14:47:12 -0400

    main reqs met, not responsive or good looking tho

--- src/assignments/project-1/components/about.js
+++ src/assignments/project-1/components/about.js
@@ -3,17 +3,31 @@ import React, { Component } from 'react'
 
 class About extends Component {
 
+	constructor(props) {
+	    super(props);
+	    this.state = {showDetails: false};
+	  }
+
+	toggleDetails() {
+		this.setState({showDetails: !this.state.showDetails})
+	}
+
 	render() {
+		const buttonText = this.state.showDetails ?  "Hide Details" : "What is this site?";
+		const {showDetails} = this.state
 
 		return (
-			<div>
-			<header>
-			<a href="">Album</a></header>
-				<h1>About the Bauhaus Postcard Photo Album</h1>
+			<section class="about">
+			<button onClick={this.toggleDetails.bind(this)}>{buttonText}</button>
+
+			<div class="details">
+				{showDetails &&
+					<div>
+				<h2>About the Bauhaus Postcard Photo Album</h2>
 				<p>Inspired by a visit to <a href="https://www.harvardartmuseums.org/visit/exhibitions/5615/the-bauhaus-and-harvard">the Bauhaus and Harvard Exhibit at Harvard Art Museums.</a>
-				</p>
-				<p>To create a postcard, visit the <a href="http://emilyc-b.com:8084/">draft postcard generator.</a></p>
-			</div>
+				To create a postcard, visit the <a href="http://emilyc-b.com:8084/">draft postcard generator.</a></p></div>}
+				</div>
+			</section>
 		);
 	}
 }
--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -1,32 +1,72 @@
-import React, { Component } from 'react'
-import Uploads from './components/uploads'
+// formerly index.js - if needed move that back!
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import Uploader from '../../ui/components/uploader'
+import ProgressBar from './components/progressbar'
+import Card from './components/card'
 import About from './components/about'
 import './app.scss'
 
-class App extends Component {
-  //set up state (to be toggled click of about/back to album)
- constructor() {
-    super();
+const Uploads = ({uploads, actions}) => {
+	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
+	const completedFiles = uploads.files.filter(({progress}) => !progress)
 
-    this.state = {
-      isUploads: true
-    }
-  }
+//Add toggle here, if showUpload == True, then show uploads : About
+	return <div class="wrapper">
+		<section class="top-set">
+			<div class="functions">
+		<h1>Bauhaus Postcard Examples</h1>
+		<h2>Upload Postcard(s)</h2>
+		{/* do not delete this uploader component */}
+		<Uploader upload={actions.upload} />
+		{/* do not delete this uploader component */}
 
-	render() {
+		<ul class="img-grid">
+			{pendingFiles.map(file => {
+				const {id, name, progress} = file
+				return <li key={id}>
+					//Progress bar component
+					<ProgressBar name={name} percentage={progress} />
+				</li>
+			})}
+		</ul>
+	</div>
+	<div>
+	<About />
 
-		return (
-      <div>
-			<Uploads />
-      </div>
-		);
-	}
+	</div>
+		</section>
+
+		<h2>Gallery:</h2>
+		<ul class="card-grid">
+			{completedFiles.map(file => {
+				//Card component called here for each uploaded image
+				return  <Card {...file} />
+			})}
+		</ul>
+	</div>
 }
 
-// const App = () => {
-//   return <div><Uploads /></div>
-// }
+const statusPropType = PropTypes.shape({
+	status: PropTypes.oneOf([`init`, `pending`, `success`, `failure`]).isRequired,
+	message: PropTypes.string.isRequired,
+})
 
+Uploads.propTypes = {
+	uploads: PropTypes.shape({
+		files: PropTypes.arrayOf(PropTypes.shape({
+			id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+			name: PropTypes.string.isRequired,
+			progress: PropTypes.number,
+			url: PropTypes.string,
+			error: PropTypes.string,
+		})).isRequired,
+		update: statusPropType.isRequired,
+		delete: statusPropType.isRequired,
+		share: statusPropType.isRequired,
+	}).isRequired,
+	actions: PropTypes.object.isRequired,
+}
 
-export default App
-// export default Uploads  //WORKS!
+export default Uploads
