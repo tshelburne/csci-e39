5f89c51766be2e97bc0e359e1fa61984405cce30
munsif.sharif <mohdmunsif@gmail.com>
2019-04-12 06:45:28 -0400

    created component card

--- src/assignments/project-1/imagecard.js
+++ src/assignments/project-1/imagecard.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
-// const ImageCard = ({ video, onVideoSelect }) => {
-const ImageCard = ( {files} ) => {
-  const {id, name, url, error} = files;
-  console.log(files);
+const ImageCard = ( { file}) => {
+  const {id, name, url, error} = file;
+  console.log("from imagecard");
   return <li key={id} className="img-key">
           <label className="img-name">{name}</label>
           {!error && <img src={url} style={{maxWidth: `200px`}} />}
           {!!error && <p className="failure">{error}</p>}
         </li>
-
-};
-
-// return <div className="one-image"> One Image </div>
+        }
 
 export default ImageCard;
