5f1fecd2ca2385f0cae0b18ffb324d40285e8118
mvartani76 <mike.vartanian@gmail.com>
2019-04-12 16:17:29 -0400

    Removed tab spacing.

--- src/assignments/project-1/album.js
+++ src/assignments/project-1/album.js
@@ -12,18 +12,18 @@ class Album extends React.Component {
 
 	render() {
 		const {photos, updateFile, ...inputProps} = this.props
-			return <ul className = "grid-container">
-				{photos.map(file => {
-					const {id, name, url, description, error} = file
-					return <li className = "card" key={id}>
-						<label className = "card-title">{name}</label>
-						{!error && <img className = "card-img" src={url} alt={description} />}
-						{!error && <Updater className="form-input" formvalue={description} file={file} updateFile={updateFile}/>}
-						{!!error && <p className="failure">{error}</p>}
-					</li>
-				})}
-			</ul>
-		}
+		return <ul className = "grid-container">
+			{photos.map(file => {
+				const {id, name, url, description, error} = file
+				return <li className = "card" key={id}>
+					<label className = "card-title">{name}</label>
+					{!error && <img className = "card-img" src={url} alt={description} />}
+					{!error && <Updater className="form-input" formvalue={description} file={file} updateFile={updateFile}/>}
+					{!!error && <p className="failure">{error}</p>}
+				</li>
+			})}
+		</ul>
+	}
 }
 
 export default Album
