1312e3b795ce44b63706c58aac11af7345d7e7e6
dave <dave@dmorg.org>
2019-04-12 13:23:02 -0400

    styling for the upload button

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -8,37 +8,40 @@ const Uploads = ({uploads, actions}) => {
 	const pendingFiles = uploads.files.filter(({progress}) => progress && progress < 100)
 	const completedFiles = uploads.files.filter(({progress}) => !progress)
 
-	return <div>
-		<h1>Upload Images</h1>
-		{/* do not delete this uploader component */}
-		<Uploader upload={actions.upload} />
-		{/* do not delete this uploader component */}
+	return (
+		<React.Fragment>
+			<label className="uploader">Click Here To Upload More Photos
+			{/* do not delete this uploader component */}
+			<Uploader className="uploader-input" upload={actions.upload} />
+			{/* do not delete this uploader component */}
+			</label>
 
-		<h2>In Progress</h2>
-		<ul>
-			{pendingFiles.map(file => {
-				const {id, name, progress} = file
+			<h2>In Progress</h2>
+			<ul>
+				{pendingFiles.map(file => {
+					const {id, name, progress} = file
 
-				return <li key={id}>
-					<label>{name}</label>
-					<progress value={progress} max="100">{progress}%</progress>
-				</li>
-			})}
-		</ul>
+					return <li key={id}>
+						<label>{name}</label>
+						<progress value={progress} max="100">{progress}%</progress>
+					</li>
+				})}
+			</ul>
 
-		<h2>Completed</h2>
-		<ul>
-			{completedFiles.map(file => {
-				const {id, name, url, error} = file
+			<h2>Completed</h2>
+			<ul>
+				{completedFiles.map(file => {
+					const {id, name, url, error} = file
 
-				return <li key={id}>
-					<label>{name}</label>
-					{!error && <img src={url} style={{maxWidth: `200px`}} />}
-					{!!error && <p className="failure">{error}</p>}
-				</li>
-			})}
-		</ul>
-	</div>
+					return <li key={id}>
+						<label>{name}</label>
+						{!error && <img src={url} style={{maxWidth: `200px`}} />}
+						{!!error && <p className="failure">{error}</p>}
+					</li>
+				})}
+			</ul>
+		</React.Fragment>
+	)
 }
 
 const statusPropType = PropTypes.shape({
