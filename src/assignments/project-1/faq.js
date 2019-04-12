e8325e61d84d6ff13590fa933be42388a7cac110
loosine <lvartani@gmail.com>
2019-04-12 14:09:47 -0400

    add Img component to Faq component

--- src/assignments/project-1/faq.js
+++ src/assignments/project-1/faq.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Img from './img'
 
 const Faq = ({name, className, content1, content2, content3}) => {
 	return <b className={className}>
@@ -8,6 +9,7 @@ const Faq = ({name, className, content1, content2, content3}) => {
 		<li>{content2}</li>
 		<li>{content3}</li>
 	</ol>
+	<Img className="faq-icon" src="http://www.ptaindia.com/wp-content/uploads/2018/10/icon-FAQ-500px.png" alt="FAQ" />
 	</b>
 };
 
