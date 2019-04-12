bb96b3d7fb91e95236f7ce813fd6c4f481550db1
mfisher29 <mafisher@mediamath.com>
2019-04-12 19:58:13 -0400

    adding edit button to polaroids, some styling

--- src/assignments/project-1/polaroidgrid.js
+++ src/assignments/project-1/polaroidgrid.js
@@ -1,17 +1,49 @@
 import React from 'react'
 
-const PolaroidGrid = ({files}) => {
-    return <div class="polaroid-grid">
-        {files.map(file => {
-            const {id, name, url, error} = file
+class PolaroidGrid extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            imgText: '',
+            buttonClicked: false,
+        }
+        this.clicked = this.clicked.bind(this);
+        this.updateImgText = this.updateImgText.bind(this)
+    }
 
+    clicked() {
+        console.log('List hidden: ' + this.state.buttonClicked);
+        this.setState(state => ({
+            buttonClicked: !state.buttonClicked
+        }));
+    }
+
+    updateImgText(newText){
+        this.setState({imgText: newText.target.value})
+        {console.log('this is the img text after an update: ' + this.imgText)}
+
+    }
+
+    render (){
+    return <div class="polaroid-grid">
+        {this.props.files.map(file => {
+            const {id, name, url, error} = file
             return <figure key={id} class="polaroid">
                 {!error && <img src={url} alt={name}/>}
                 {!!error && <p className="failure">{error}</p>}
-                <figcaption class="polaroid-caption">{name}</figcaption>
+                
+                <figcaption class="polaroid-caption">{name.split('.').slice(0, -1).join('.')}</figcaption>
+                <button class="edit-button" onClick={ (e) => { this.clicked(); } }>{ this.state.buttonClicked ? 'Save' : 'Edit' }</button>
+                {this.state.buttonClicked && <form>
+                    <label>
+                        <input type="text" value={this.imgText} onChange={this.updateImgText} />
+                    </label>
+                </form>
+                }
             </figure>
         })}
     </div>
+    }
 }
 
-export default PolaroidGrid
+export { PolaroidGrid as default};
