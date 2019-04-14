4c0235115af278d6bcfa1600e9ffdcf8a6d38c80
acomjean <acomjean@gmail.com>
2019-04-14 09:10:43 -0400

    make textbox component

--- src/assignments/project-2/index.js
+++ src/assignments/project-2/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import autobind from 'class-autobind'
 import Member from '../../ui/components/member'
-
+import Textbox from '../../ui/components/textbox'
 import './app.scss'
 
 class Chat extends React.Component {
@@ -13,40 +13,6 @@ class Chat extends React.Component {
 		this.state = {currentText: ``}
 	}
 
-	onType(e) {
-		const {chat} = this.props.actions
-		const {currentText: prevText} = this.state
-		const currentText = e.target.value
-
-		if (!currentText.length) chat.stopTyping()
-		if (currentText.length === 1 && !prevText.length) chat.startTyping()
-
-		this.setState({currentText})
-	}
-
-	onSend(e) {
-		if (e.type === `keyup` && e.key !== `Enter`) return
-
-		const {chat} = this.props.actions
-		const {currentText} = this.state
-		if (!currentText.length) return
-
-		chat.send(currentText)
-		this.setState({currentText: ``})
-	}
-
-	getTypingMessage() {
-		const {typing} = this.props.chat
-
-		switch (typing.length) {
-			case 0: return null
-			case 1: return `${typing[0].name} is typing...`
-			case 2: return `${typing[0].name} and ${typing[1].name} are typing...`
-			case 3: return `${typing[0].name}, ${typing[1].name}, and ${typing[2].name} are typing...`
-			// len > 3
-			default: return `${typing.length} members are typing...`
-		}
-	}
 
 	render() {
 		const {classroom, chat, actions} = this.props
@@ -75,9 +41,7 @@ class Chat extends React.Component {
 			{/*</div>*/}
 			
 			<footer>
-				<input value={currentText} onChange={this.onType} onKeyUp={this.onSend} />
-				<button disabled={currentText === ``} onClick={this.onSend}>Send</button>
-				<p>{this.getTypingMessage()}</p>
+				<Textbox chat={chat} actions={actions}/>
 			</footer>
 			
 		</div>
