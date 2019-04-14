95271911a349299bd65eb22806174601e027b998
matt <matthewjkim@gmail.com>
2019-04-14 11:30:33 -0400

    add ads and changes images

--- src/assignments/project-2/components/ads/ads.js
+++ src/assignments/project-2/components/ads/ads.js
@@ -12,8 +12,8 @@ export default class Ads extends React.Component {
   render() {
     return (
       <List>
-        <Ad img={"http://www.fillmurray.com/150/150"} text = {"Free Golden Company with loan of 10,000 gold dragons"}/>
-        <Ad img={"http://www.fillmurray.com/150/150"} text = {"Need a career change? Want to defend the Realms of Men? Join the Night's Watch!"}/>
+        <Ad img={"https://img-9gag-fun.9cache.com/photo/aWZ53yK_700bwp.webp"} text = {"Free Golden Company with loan of 10,000 gold dragons"}/>
+        <Ad img={"https://vignette.wikia.nocookie.net/gameofthrones/images/e/ed/Night%27s-Watch-Main-Shield.PNG/revision/latest?cb=20161231113156"} text = {"Need a career change? Want to defend the Realms of Men? Join the Night's Watch!"}/>
       </List>
     );
   }
