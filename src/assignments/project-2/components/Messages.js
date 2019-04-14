028678166be42fae43483a866ea53944ac71366f
adamshapiro <adam.shapiro595@gmail.com>
2019-04-14 10:56:06 -0400

    restyled messages for messages from self

--- src/assignments/project-2/components/Messages.js
+++ src/assignments/project-2/components/Messages.js
@@ -8,8 +8,8 @@ function Messages({ messages, self }) {
       <ul className="message-list">
         {messages.map(({ id, student, text, createdAt }) =>
         <li className={((student.id == self.id) ? "message-content self" : "message-content")} key={id} >
-            <label>{student.name}</label>
-            <span className="message-text">{text}</span>
+            <label className="message-name">{student.name}</label>
+            <p className="message-text">{text}</p>
             <small className="message-timestamp">
                 {createdAt.getHours()}:{(createdAt.getMinutes() < 10) ? ("0" + createdAt.getMinutes()) : createdAt.getMinutes()} on {createdAt.toLocaleDateString()}
             </small>
