b74e036f787c5018696a36a1cf2453bead2cf405
terichadbourne <terichad@gmail.com>
2019-04-22 23:40:41 -0400

    add countFavorites

--- src/assignments/project-1/components/gallery.js
+++ src/assignments/project-1/components/gallery.js
@@ -8,10 +8,12 @@ class Gallery extends React.Component {
 		super(...arguments)
 		autobind(this)
 		this.state = {
-			isFiltered: false
+			isFiltered: false,
+			hasFavorites: false
 		}
 		// This binding is necessary to make `this` work in the callback
 		this.toggleFiltered = this.toggleFiltered.bind(this);
+		this.countFavorites = this.countFavorites.bind(this);
 	}
 
 	toggleFiltered() {
@@ -20,16 +22,28 @@ class Gallery extends React.Component {
      }));
   }
 
+	countFavorites() {
+		const favoritesCount = document.querySelectorAll('.card[data-favorite="true"]').length
+		const hasFavorites = favoritesCount > 0
+		console.log("favoritescount is ", favoritesCount)
+		this.setState(state => ({
+			hasFavorites: hasFavorites
+		}));
+		console.log("this.state.hasFavorites is ", this.state.hasFavorites)
+	}
+
 	render() {
 		const { successfulFiles } = this.props
+		// const hasFavorites = document.querySelectorAll('.card[data-favorite="true"]').length > 0 ? true : false
 		return <div>
 			<h2>Gallery</h2>
-			<p>My goodness, you're looking good!</p>
+			<p>My goodness, you are looking good!</p>
+			{this.state.hasFavorites && <p>there are favorites so there should be a filter button</p>}
 			<button onClick={this.toggleFiltered} className="filtered-button" aria-label={this.state.isFiltered ? "Show all" : "Show only favorites"} data-filtered={this.state.isFiltered} > {this.state.isFiltered ? "Show all" : "Show only favorites"}</button>
-			<ul class="gallery" data-filtered={this.state.isFiltered}>
+			<ul onClick={this.countFavorites} class="gallery" data-filtered={this.state.isFiltered}>
 				{successfulFiles.map(file => {
 					const {id, name, url, error} = file
-					return <Card { ...file } />
+					return <Card { ...file  } />
 				})}
 			</ul>
 		</div>
