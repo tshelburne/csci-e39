538acb05d449f51664b8da1ed9060c69b01d57fa
Bob Frasure <bobsdesigns@hotmail.com>
2019-04-27 13:55:07 -0700

    Moved back to project 1
    I am really concerned about the condition of project 1 in the repository. It looks like it got messed up during the code camp weekend. I've attempted to point this back to project-1

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Module from '../assignments/project-2'
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 27ef070742bc7a15ae90177c3b6fdab5bd6aa0b0
+import Module from '../assignments/project-1' 
 const App = ({auth, ...props}) => {
 	switch (auth.status) {
 		case `init`: return <span>Authorizing</span>
