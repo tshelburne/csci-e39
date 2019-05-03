e3dae8a1a759cb7a1280150d17ccd2c2a57aae59
Emily Bottis <ebottis4@gmail.com>
2019-05-02 21:35:28 -0400

    added props to descriptions

--- src/assignments/design-patterns/index.js
+++ src/assignments/design-patterns/index.js
@@ -15,24 +15,59 @@ const PatternLibrary = () =>
 
 			<Example title="Section (H2 heading) <Section>">
 				<Section title='H2 Heading' styleName='chat-members'></Section>
-				<div className="just-testing"><p>This is a child paragraph.  The H2 is the default level - you don't need to pass a 'level' prop to get a H2.</p></div>
+				<div className="just-testing"><p>Section with a configurable heading.  Takes children.</p>
+				<ul>Props:
+					<li>Title [string, required]</li>
+					<li>Heading Level [string], values ('2', '3', '4', '5', '6'), default = 2</li>
+				</ul>
+				</div>
 			</Example>
 
 			<Example title="Section (H4 heading) <Section>">
 				<Section title='H4 Heading' styleName='chat-members' headingLevel='4'></Section>
-				<div className="just-testing"><p>This is a child paragraph.</p></div>
+				<div className="just-testing"><p>Section with H4 example</p></div>
 			</Example>
 
 			<Example title="Numeric Input (Top Label) <Input>">
 			<Input inputName="years" labelText="Years employed:" labelPosition="top" inputType="number" ></Input>
+			<div className="just-testing"><p>Configurable input. Does not accept children.</p>
+				<ul>Props:
+					<li>
+					InputName:  Name of the input to associate the label For attribute [string, required]
+					</li>
+					<li>
+					labelText:  Text value of the label.  [string, required]
+					</li>
+					<li>
+					labelPosition:  values ('top', 'left'), default = 'top'
+					</li>
+					<li>
+					inputType:  values ('text', 'number', 'email'), default = 'text'
+					</li>
+				</ul>
+				</div>
 			</Example>
 
 			<Example title="Text Input (Left Label) <Input>">
 			<Input inputName="firstName" labelText="First Name:" labelPosition="left" inputType="text" ></Input>
+			<div className="just-testing"><p>Another example of the input.</p></div>
 			</Example>
 
 			<Example title="Signup Form <Signup>">
 			<Signup labelText="Get our weekly newsletter!" buttonText="Let's Go!" labelPosition="left"></Signup>
+			<div className="just-testing"><p>Email signup form. Does not accept children.  Uses the Input Component</p>
+				<ul>Props:
+					<li>
+					buttonText: [string], default = 'Sign up!',
+					</li>
+					<li>
+					labelPosition: values ('top', 'left)', default = 'top'
+					</li>
+					<li>
+					labelText: [string], default="Signup for our newsletter!"					
+					</li>
+				</ul>
+				</div>
 			</Example>
 		</div>
 	</ActiveCodeProvider>
