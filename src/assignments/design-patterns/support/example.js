0e1a13edeac887dacecf7d6fb32102ba7eff5708
jkhambay <35969821+jkhambay@users.noreply.github.com>
2019-05-19 22:14:29 -0400

    Add files via upload

--- src/assignments/design-patterns/support/example.js
+++ src/assignments/design-patterns/support/example.js
@@ -1,25 +1,12 @@
-import React, {useState, useContext} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
-import cx from 'classnames'
 import jsxToString from 'jsx-to-string'
 import beautify from 'js-beautify'
 
-const ActiveCodeContext = React.createContext()
-
-export const ActiveCodeProvider = ({children}) => {
-	const [activeCode, setActiveCode] = useState(`react`)
-
-	return <ActiveCodeContext.Provider value={{activeCode, setActiveCode}}>
-		{children}
-	</ActiveCodeContext.Provider>
-}
-
-const Example = ({title, children}) => {
-	const {activeCode, setActiveCode} = useContext(ActiveCodeContext)
-
-	return <div className="example">
+const Example = ({title, children}, context) => (
+	<div className="example">
 		<h2 className="example--title">{title}</h2>
 
 		<div className="example--rendered">
@@ -29,53 +16,55 @@ const Example = ({title, children}) => {
 
 		<div className="example--toggles">
 			<button
-				onClick={() => setActiveCode(`html`)}
-				className={cx({active: activeCode === `html`})}
+				onClick={() => context.setActiveCode(`html`)}
+				className={context.activeCode === `html` ? `active` : ``}
 			>
 				HTML
 			</button>
 			<button
-				onClick={() => setActiveCode(`react`)}
-				className={cx({active: activeCode === `react`})}
+				onClick={() => context.setActiveCode(`react`)}
+				className={context.activeCode === `react` ? `active` : ``}
 			>
 				React
 			</button>
 			<button
-				onClick={() => setActiveCode(`off`)}
-				className={cx({active: activeCode === `off`})}
+				onClick={() => context.setActiveCode(`off`)}
+				className={context.activeCode === `off` ? `active` : ``}
 			>
 				Off
 			</button>
 		</div>
 
-		{activeCode !== `off` &&
+		{context.activeCode !== `off` && 
 			<div className="example--code">
 				<h3>Code sample:</h3>
 				<pre>
 					<code>
-						{activeCode === `html` && renderHTML(children)}
-						{activeCode === `react` && renderReactCode(children)}
+						{context.activeCode === `html` &&
+							beautify
+								.html(ReactDOMServer.renderToStaticMarkup(<div>{children}</div>))
+								.replace(/^<div>.*\n([\S\s]*)<\/div>$/, `$1`)
+						}
+						
+						{context.activeCode === `react` &&
+							jsxToString(<div>{children}</div>)
+								.replace(/=\{true\}/g, ``)
+								.replace(/^<div>.*\n([\S\s]*)<\/div>$/, `$1`)
+						}
 					</code>
 				</pre>
 			</div>
 		}
 	</div>
-}
+)
 
 Example.propTypes = {
 	title: PropTypes.string,
 }
 
+Example.contextTypes = {
+	activeCode: PropTypes.string,
+	setActiveCode: PropTypes.func,
+}
+
 export default Example
-
-// HELPERS
-
-function renderHTML(children) {
-	return beautify.html(ReactDOMServer.renderToStaticMarkup(<React.Fragment>{children}</React.Fragment>))
-}
-
-function renderReactCode(children) {
-	return jsxToString(<div>{children}</div>)
-		.replace(/=\{true\}/g, ``)
-		.replace(/^<div>.*\n([\S\s]*)<\/div>$/, `$1`)
-}
