2d8724a015d13c055ad7fd9a41e20568bd2db562
JUDY J MILLER <jjm@JUDYs-Air.fios-router.home>
2019-05-20 05:02:08 -0400

    what is with the scss file

--- src/assignments/project-1/index.js
+++ src/assignments/project-1/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Uploader from '../../ui/components/uploader'
+
 import './app.scss'
 
 const Uploads = ({uploads, actions}) => {
